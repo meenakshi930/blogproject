1c4ca65a4f6529acb5e2d202beb88df4ff7ff8a4
meenakshi930 <meenakshi.gupta_cs.aiml24@gla.ac.in>
2026-04-25 15:41:49 +0530

    commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -120,3 +120,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+-e 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
